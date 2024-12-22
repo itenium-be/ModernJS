489e538c3e948348305229199c330fe58274f9ad
Wouter Van Schandevijl <woutervs@hotmail.com>
2024-12-22 19:20:56 +0100

    much simpler way to strongly type a const array

--- src/TypeScript/4.Enums.spec.ts
+++ src/TypeScript/4.Enums.spec.ts
@@ -12,11 +12,12 @@ describe('You may not need/want an enum', () => {
 
 describe('create a type from an array', () => {
   const gameStates = ['start', 'running', 'end'] as const;
-  type ElementType <T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer ElementType>
-    ? ElementType
-    : never;
+  // type ElementType <T extends ReadonlyArray<unknown>> = T extends ReadonlyArray<infer ElementType>
+  //   ? ElementType
+  //   : never;
 
-  type GameStates = ElementType<typeof gameStates>;
+  // type GameStates = ElementType<typeof gameStates>;
+  type GameStates = typeof gameStates[number];
 
   let gameState: GameStates = 'start';
   gameState = 'end';
